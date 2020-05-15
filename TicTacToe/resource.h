4417d77dfc2e0b083d66587d4d1fdca3c08c76da
Emila <emiliastarczyk@gmail.com>
2020-05-15 11:40:01 +0100

    Move resources.

--- TicTacToe/resource.h
+++ TicTacToe/resource.h
@@ -2,12 +2,20 @@
 // Plik do³¹czany wygenerowany przez œrodowisko Microsoft Visual C++.
 // U¿ywany przez: resource.rc
 //
+#define IDB_BITMAP1                     103
+#define IDB_BITMAP2                     104
+#define IDB_BITMAP3                     105
+#define IDB_BITMAP4                     106
+#define IDB_BITMAP5                     107
+#define IDB_BITMAP6                     108
+#define IDB_BITMAP7                     109
+#define IDB_BITMAP8                     110
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_RESOURCE_VALUE        111
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
